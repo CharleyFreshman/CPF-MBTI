4ff79d9b0ffa11cc24a79ef0b11d96a158c73ee5
Sifan Song <sifansong432@qq.com>
2026-09-01 21:44:01 +0800

    Update: 2026-09-01T13:44:01.434Z

--- 上传.bat
+++ 上传.bat
@@ -1,8 +1,8 @@
 @echo off
 chcp 65001 >nul
 echo ===== 一键上传 GitHub =====
-echo 目标: mbti / magicbook / expedition （不带参数=全部上传）
+echo 目标: mbti / magicbook / expedition （ %* 则全部上传）
 cd /d "d:\Trae CN\Garth"
-node "d:\Trae CN\Garth\upload-to-github.js" %*
+node "d:\Trae CN\Garth\upload-to-github.js" mbti
 echo.
 pause
